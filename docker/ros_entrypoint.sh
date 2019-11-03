5d592825427768dbc4ba3947e44927acd6876c22
Minyoung Kim <mkim@csl.sri.com>
2019-11-02 22:32:42 -0700

    ros1 docker

---
+++ docker/ros_entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# setup ros environment
+source "$ROS_PREFIX/$ROS_DISTRO/setup.bash"
+
+# build radler pubsub example
+cd ~/radler 
+git pull
+./radler.sh --ws_dir $ROS_WS/src compile examples/pubsub/multi_machine/pubsub.radl --plant plant --ROS
+cd $ROS_WS
+catkin_make
+source "$ROS_WS/devel/setup.bash"
+exec "$@"
