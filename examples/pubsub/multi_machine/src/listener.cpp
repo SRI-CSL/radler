7dea145c7a2c39fd6c75d73c83066fe23ec07caa
Minyoung Kim <mkim@csl.sri.com>
2019-11-02 23:38:30 -0700

    minor fix to pubsub

--- examples/pubsub/multi_machine/src/listener.cpp
+++ examples/pubsub/multi_machine/src/listener.cpp
@@ -2,13 +2,10 @@
 #include <iostream>
 
 Listener::Listener() {
-  this->counter = 0;
 }
 
 void Listener::step(const radl_in_t * in, const radl_in_flags_t* inflags,
                       radl_out_t * out, radl_out_flags_t* outflags){
-  if ( this->counter++ % 100 == 0 ) {
+  if ( !radl_is_stale(inflags->chatter) && !radl_is_timeout (inflags->chatter) )
     std::cout << "Received " << in->chatter->data << std::endl;
-  }
 }
-
