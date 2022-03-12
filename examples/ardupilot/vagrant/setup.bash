a5b5263689114569401df4574cf1551193fce3ea
Minyoung Kim <mkim@csl.sri.com>
2022-03-11 21:47:19 -0800

    setup bash

--- examples/ardupilot/vagrant/setup.bash
+++ examples/ardupilot/vagrant/setup.bash
@@ -1,4 +1,4 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/jvm/java-11-openjdk-amd64/lib/server
 export DAIKONDIR=/home/vagrant/daikon-5.8.10
-source $DAIKONDIR/scripts/daikon.bashrc"
+source $DAIKONDIR/scripts/daikon.bashrc
 export CLASSPATH=$CLASSPATH:/home/vagrant/daikon-5.8.10/java/daikon:/home/vagrant/radler/examples/ardupilot/jni:/home/vagrant/beepbeep-3-examples/beepbeep-3-examples.jar:/home/vagrant/daikon-5.8.10/daikon.jar:/home/vagrant/daikon-5.8.10/java/dcomp_rt.jar:/home/vagrant/daikon-5.8.10/java/ChicoryPremain.jar:/home/vagrant/daikon-5.8.10/java/dcomp_premain.jar
